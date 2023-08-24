34db11ee4bc2088b6b81f9de72b8238d802b230c
Helia Gohar <helia77@users.noreply.github.com>
2023-08-24 11:28:18 -0500

    print all lines

--- vec_to_streamline.py
+++ vec_to_streamline.py
@@ -12,7 +12,6 @@ The vector field is basically the two (three for 3D volume) eigenvectors calcula
 #%%
 
 import numpy as np
-from numpy import linalg
 import scipy.linalg as lin
 import matplotlib.pyplot as plt
 #%%
@@ -23,7 +22,7 @@ import matplotlib.pyplot as plt
     # iters:        number of iterations for the Euler integration
     # epsilon:      smallest value to compare the smallest possible vector size with
     
-def vec2streamline_2d(vec_field, seed_pts, img_range, iters = 1000, epsilon = 0.01):
+def vec2streamline_2d(vec_field, seed_pts, img_range, iters = 10000, epsilon = 0.1):
     all_lines = []
     for i in range(len(seed_pts)):
         line = []
@@ -62,10 +61,10 @@ def vec2streamline_2d(vec_field, seed_pts, img_range, iters = 1000, epsilon = 0.
     
 #%%
 
-# create a face 2D vector field
+# create a 2D vector field
 N = 100
-xx = np.linspace(-10, 10, N)
-X, Y = np.meshgrid(xx, xx)
+x_range = np.linspace(-10, 10, N)
+X, Y = np.meshgrid(x_range, x_range)
 R = np.sqrt(X**2 + Y**2)
 dx, dy = np.gradient(R)
 
@@ -83,9 +82,8 @@ vec_field = np.empty((T.shape[0], T.shape[1], 2))
 for i in range(T.shape[0]):
     for j in range(T.shape[1]):
         tensor_field = T[i, j, :, :]
-        eigvals, eigvecs = lin.eigh(tensor_field)                # sorted
-        smallest_eigvec = eigvecs[:, 0]
-        vec_field[i, j] = smallest_eigvec
+        eigvals, eigvecs = lin.eigh(tensor_field)                   # sorted
+        vec_field[i, j] = eigvecs[:, 0]                             # smallest eigenvector
         
 #%%
 
@@ -104,29 +102,40 @@ plt.title('Streamplot Example')
 
 # Show the plot
 plt.show()
+
 #%%
 
+#num_points = 20
+#seed_pts = [[-2.5, -2.5], [-2.5, 0], [-2.5, 2.5], [0, -2.5], [0, 2.5], [2.5, -2.5], [2.5, 0], [2.5, 2.5]]
 num_points = 20
-seed_pts = [[-2.5, -2.5], [-2.5, 0], [-2.5, 2.5], [0, -2.5], [0, 2.5], [2.5, -2.5], [2.5, 0], [2.5, 2.5]]
 
+angles = np.random.uniform(0, 2 * np.pi, num_points)
 
-# Plot the meshgrid and the random seed points
+radius = 5  # Adjust this radius as needed
+x_coordinates = radius * np.cos(angles)
+y_coordinates = radius * np.sin(angles)
+
+seed_pts = []
+for x, y in zip(x_coordinates, y_coordinates):
+    if -5 <= x <= 5 and -5 <= y <= 5:
+        seed_pts.append((x, y))
+
 plt.figure(figsize=(8, 8))
-#plt.imshow(np.zeros_like(X), extent=(-10, 10, -10, 10), cmap='gray')
-#plt.scatter(*zip(*seed_pts), color='red', label='Seed Points')
+plt.imshow(np.zeros_like(X), extent=(-10, 10, -10, 10), cmap='gray')
+plt.scatter(*zip(*seed_pts), color='red', label='Seed Points')
 plt.legend()
 plt.xlim(-10, 10)
 plt.ylim(-10, 10)
 #plt.show()
-
+#%%
 img_range = [[-10, 10], [-10, 10]]
 all_lines = vec2streamline_2d(vec_field, seed_pts, img_range)
+#plt.scatter(*zip(*all_lines[1]), color='blue', marker='.', label='Seed Points')
+#plt.show()
 
-line = all_lines[7]
-#plt.plot(*zip(*all_lines[0]), "r--", *zip(*all_lines[1]), "b..")
-plt.scatter(*zip(*all_lines[0]))
-plt.show()
-
+for i in range(num_points):
+    plt.scatter(*zip(*all_lines[i]), color='blue', marker='.', label='Seed Points')
+    plt.show()
 
 
 
